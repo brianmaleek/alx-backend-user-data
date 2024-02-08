ca65aabe04eab2daed1ccfd29919ef34338fe1b0
maleekmalik@gmail.com <maleekmalik@gmail.com>
2024-02-09 02:25:30 +0300

    6. Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,3 +14,21 @@ def hash_password(password: str) -> bytes:
     encode = password.encode()
     hashed = bcrypt.hashpw(encode, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates whether the provided password matches the hashed password.
+
+    Args:
+    hashed_password (bytes): The salted, hashed password.
+    password (str): The password to validate.
+
+    Returns:
+    bool: True if the password matches the hashed password, False otherwise.
+    """
+    valid = False
+    encode = password.encode()
+    if bcrypt.checkpw(encode, hashed_password):
+        valid = True
+    return valid
