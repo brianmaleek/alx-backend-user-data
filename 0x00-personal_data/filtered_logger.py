aa85013435d397333b32b723310a62256cf92563
maleekmalik@gmail.com <maleekmalik@gmail.com>
2024-02-09 00:00:20 +0300

    0. Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+- Description: function called filter_datum returns the log message obfuscated:
+- Arguments:
+    - fields: a list of strings representing all fields to obfuscate
+    - redaction: a string representing by what the field will be obfuscated
+    - message: a string representing the log line
+    - separator: a string representing by which character is separating all
+        fields in the log line (message)
+- function should use a regex to replace occurrences of certain field values.
+- filter_datum should be less than 5 lines long and use re.sub to perform the
+    substitution with a single regex.
+"""
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str],
+                 redaction: str, message: str, separator: str) -> str:
+    for field in fields:
+        message = re.sub(fr'{re.escape(field)}=.+?{re.escape(separator)}',
+                         f'{field}={redaction}{separator}', message)
+    return message
