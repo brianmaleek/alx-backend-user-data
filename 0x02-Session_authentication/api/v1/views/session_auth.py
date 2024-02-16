b5a53cd2a51992d7f09b55bcb7a1362bbfbb987c
brianmaleek <maleekmalik@gmail.com>
2024-02-16 05:25:47 +0300

    7. New view for Session Authentication - checker fix 7,  10, 12, 13

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -2,7 +2,7 @@
 """ Module of session auth views
 """
 from api.v1.views import app_views
-from flask import abort, jsonify, request
+from flask import abort, jsonify, request, make_response
 from models.user import User
 from os import getenv
 
@@ -13,7 +13,7 @@ def session_login():
     Handle user login and session creation.
 
     This route handles the user login process by receiving the email and
-    password. parameters from the request form data. It validates the provided
+    password parameters from the request form data. It validates the provided
     credentials and creates a session ID for the authenticated user.
 
     Returns:
@@ -24,22 +24,31 @@ def session_login():
     user_password = request.form.get("password")
 
     # Check if email and password are provided
-    if user_email is None or user_email == "":
+    if not user_email:
         return jsonify({"error": "email missing"}), 400
-    if user_password is None or user_password == "":
+    if not user_password:
         return jsonify({"error": "password missing"}), 400
 
     # Retrieve user by email
-    is_valid_user = User.search({"email": user_email})
-    if not is_valid_user:
+    user = User.search({"email": user_email})
+    if not user:
         return jsonify({"error": "no user found for this email"}), 404
 
     # Check if password is valid
-    if not is_valid_user.is_valid_password(user_password):
+    if not user.is_valid_password(user_password):
         return jsonify({"error": "wrong password"}), 401
 
     from api.v1.app import auth
-    session_id = auth.create_session(is_valid_user.id)
-    response_data = getenv(("SESSION_NAME"), session_id)
-    response_dict = jsonify(is_valid_user.to_dict())
-    return response_dict.set_cookie(response_data, session_id)
+    session_id = auth.create_session(user.id)
+
+    # Retrieve session name from environment variable or use a default value
+    session_name = getenv("SESSION_NAME", "_my_session_id")
+
+    # Create response with user data
+    response_dict = jsonify(user.to_dict())
+
+    # Set session cookie
+    response = make_response(response_dict)
+    response.set_cookie(session_name, session_id)
+
+    return response
