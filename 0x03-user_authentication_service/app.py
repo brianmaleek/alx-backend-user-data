104f5594476e77fce545a0f86feb7f97db7115d1
maleekmalik@gmail.com <maleekmalik@gmail.com>
2024-02-23 02:39:17 +0300

    app.py -- 7. Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,12 @@
 """
 Basic Flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -14,6 +16,30 @@ def index() -> str:
     Main route for the Flask app
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user() -> str:
+    """
+    Register a user
+
+    Returns:
+        str: JSON string
+
+    Raises:
+        ValueError: If the user already exists
+    """
+    # get the email and password from the data
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        # if the user does not exist, register the user
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        # if the user already exists, return a 400 error
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
