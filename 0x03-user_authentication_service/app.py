3e5e3da8b25709650a738d30a5f6cb2d7297253b
maleekmalik@gmail.com <maleekmalik@gmail.com>
2024-02-23 04:00:26 +0300

    app.py -- 11. Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Basic Flask app
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response, abort
 from auth import Auth
 
 
@@ -40,6 +40,40 @@ def register_user() -> str:
     except ValueError:
         # if the user already exists, return a 400 error
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """
+    Handle user login.
+
+    - Expects a POST request with form data containing "email" and "password"
+        fields.
+
+    - Creates a new session for the user, stores the session ID as a cookie,
+        and returns a JSON response.
+
+    Returns:
+        Flask response: JSON response confirming the login.
+
+    Raises:
+        - HTTPException: If login information is incorrect, aborts with a 401
+            status code.
+    """
+    # get the email and password from the data
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # if the user exists and the password is valid, create a new session
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = make_response(jsonify({"email": email, "message":
+                                          "logged in"}))
+        response.set_cookie("session_id", session_id)
+        return response
+    # if the user does not exist or the password is invalid, return a 403 error
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
