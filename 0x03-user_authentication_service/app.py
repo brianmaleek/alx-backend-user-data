bd8bfd526c48a46f19f610aadacf55809f5ed79c
maleekmalik@gmail.com <maleekmalik@gmail.com>
2024-02-23 05:01:54 +0300

    app.py -- 17. Get reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -113,5 +113,21 @@ def profile() -> str:
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """
+    Get the reset password token
+
+    Returns:
+        str: JSON string
+    """
+    try:
+        email = request.form.get('email')
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
